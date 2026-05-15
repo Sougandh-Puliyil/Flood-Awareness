7c3419aec9a4fe589ee0bdeeb286091ad2273e3e
Sougandh <sougandhpuliyil@gmail.com>
2026-05-15 21:20:02 +0530

    error fix try

--- volume_reg.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-@onready var volume_slider = $VolumeSlider
-
-func _ready():
-	volume_slider.connect("value_changed", _on_volume_changed)
-
-func _on_volume_changed(value):
-	AudioServer.set_bus_volume_db(0, linear_to_db(value))
