2d3b832cdd8aeb60ed51a3f03283a6c7f0e25f30
Sougandh <sougandhpuliyil@gmail.com>
2026-05-16 13:03:22 +0530

    UI elements added from UI branch

---
+++ main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var popup = $ProfilePopup
+
+func _ready():
+	popup.visible = false
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://ui/secondpage.tscn")
+
+func _on_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://ui/Scorecard.tscn")
+
+func _on_ProfileButton_pressed():
+	popup.visible = popup.visible
