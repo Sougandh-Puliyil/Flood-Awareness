db841e25fa856dc85813c98d875566bb461b19c5
Vismaya T R <150802586+MPKDH-251@users.noreply.github.com>
2026-05-17 16:39:54 +0530

    Debugged UI and Database Integration

--- ui/oldmain_menu.gd
+++ ui/oldmain_menu.gd
@@ -1,15 +1,32 @@
 extends Control
 
-@onready var popup = $ProfilePopup
+@onready var profile_dropdown = %ProfileDropdown
+@onready var username_label = %UsernameLabel
 
 func _ready():
-	popup.visible = false
-
+	# Update the name dynamically from your Global script or Database
+	if Global_Logic.player_username != "":
+		username_label.text = Global_Logic.player_username
+	else:
+		get_tree().change_scene_to_file("res://ui/frontpage.tscn")
+	
+	# Ensure dropdown is hidden on start
+	profile_dropdown.visible = false
+	
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://ui/secondpage.tscn")
 
+
 func _on_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://ui/Scorecard.tscn")
+	
+func _on_profile_icon_pressed():
+	profile_dropdown.visible = !profile_dropdown.visible
+	
 
-func _on_ProfileButton_pressed():
-	popup.visible = popup.visible
+func _on_logout_pressed():
+	print("Logging out user: ", Global_Logic.player_username)
+	
+	var status = AuthorizationManager.logout()
+	if status.success:
+		get_tree().change_scene_to_file("res://ui/frontpage.tscn") 		#  Redirect to Front page scene
